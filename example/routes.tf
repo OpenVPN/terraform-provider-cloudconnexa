1f0039cf6759c4e5d0874ef14c024429f9db0289
Ofir Shtrull <shtrull@gmail.com>
2024-05-09 19:29:24 +0300

    Update routes.tf

--- example/routes.tf
+++ example/routes.tf
@@ -1,9 +1,9 @@
 resource "cloudconnexa_route" "this" {
   for_each = {
-    for key, route in var.routes : route.value => route
+    for key, route in var.routes : route.subnet => route
   }
   network_item_id = var.networks["example-network"]
   type            = "IP_V4"
-  subnet           = each.value.value
+  subnet           = each.value.subnet
   description     = each.value.description
 }
