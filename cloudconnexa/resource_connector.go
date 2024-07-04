aa31e7177456e68c212cae8c00cc526c8171edbd
michaelfmnk <michael.fmnk@gmail.com>
2024-07-04 12:21:56 +0200

    update connector implemented

--- cloudconnexa/resource_connector.go
+++ cloudconnexa/resource_connector.go
@@ -16,6 +16,7 @@ func resourceConnector() *schema.Resource {
 		CreateContext: resourceConnectorCreate,
 		ReadContext:   resourceConnectorRead,
 		DeleteContext: resourceConnectorDelete,
+		UpdateContext: resourceConnectorUpdate,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
@@ -62,6 +63,23 @@ func resourceConnector() *schema.Resource {
 			},
 		},
 	}
+}
+
+func resourceConnectorUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*cloudconnexa.Client)
+	var diags diag.Diagnostics
+	connector := cloudconnexa.Connector{
+		Id:              d.Id(),
+		Name:            d.Get("name").(string),
+		NetworkItemId:   d.Get("network_item_id").(string),
+		NetworkItemType: d.Get("network_item_type").(string),
+		VpnRegionId:     d.Get("vpn_region_id").(string),
+	}
+	_, err := c.Connectors.Update(connector)
+	if err != nil {
+		return append(diags, diag.FromErr(err)...)
+	}
+	return diags
 }
 
 func resourceConnectorCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
