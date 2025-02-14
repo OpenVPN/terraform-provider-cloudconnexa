fa47035bbfd14413ca9c2a137d4f4d42f2218127
Olexandr Lytvyn <saha338-aa@yahoo.com>
2025-02-14 16:21:38 +0200

    Add extra context for importing cloudconnxa_network

--- examples/resources/cloudconnexa_network/import.sh
+++ examples/resources/cloudconnexa_network/import.sh
@@ -1,1 +1,3 @@
+# Importing resource "cloudconnexa_network" currently is not supported.
+# You may use as workaround data source "cloudconnexa_network" to reference it in code.
 terraform import cloudconnexa_network.example <id>
